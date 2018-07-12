09b0ff3814a331af1804216e9ce77fe1fa1ae380
dfahlander <david.fahlander@gmail.com>
2018-07-12 16:43:24 +0200

    Some more tests

--- src/core/create-entity-proxy.test.ts
+++ src/core/create-entity-proxy.test.ts
@@ -2,6 +2,8 @@ import { EntityProxy } from "./entity-proxy";
 import { createProxy } from "./create-entity-proxy";
 import { JsExpression } from "./js-expression";
 import { dataExpression } from "../symbols";
+import { NOT } from "./not";
+import { Expression } from "./expression";
 
 interface Friend {
   name: string;
@@ -15,12 +17,12 @@ interface Car {
   model: string;
 }
 
-function verify<T> (jsExpr: JsExpression<T>, expectedResult: any[]) {
+function verify<T>(jsExpr: JsExpression<T>, expectedResult: any[]) {
   const data = jsExpr(createProxy<T>())[dataExpression];
   expect(data).toEqual(expectedResult);
 }
 
-test('create-entity-proxy', ()=>{
+test('create-entity-proxy', () => {
 
   verify<Friend>(
     friend => friend.age.above(25), [
@@ -29,9 +31,9 @@ test('create-entity-proxy', ()=>{
       25
     ]);
 
-  verify<Friend> (
+  verify<Friend>(
 
-    ({name}) =>
+    ({ name }) =>
       name.startsWithAnyOf(["D", "Y"]).ignoreCase(),
 
     [
@@ -42,64 +44,96 @@ test('create-entity-proxy', ()=>{
     ]
   );
 
-  verify<Friend>(({name, cars}) =>
+  verify<Friend>(({ name, cars }) =>
     name.anyOf(["Arnold", "Hulken"]).AND(
-      cars.some(car => car.brand.equals("Volvo"))) ,
-      
-  [
-    [
-      "name",
-      "anyOf",
-      ["Arnold", "Hulken"]    
-    ],
-    "AND",
-    [
-      "cars",
-      "some",
-      [
-        "brand",
-        "equals",
-        "Volvo"
-      ]
-    ]
-  ]);
+      cars.some(car => car.brand.equals("Volvo"))),
 
-  verify<Friend>(({name, tags, age, cars}) => 
+    [
+      [
+        "name",
+        "anyOf",
+        ["Arnold", "Hulken"]
+      ],
+      "AND",
+      [
+        "cars",
+        "some",
+        [
+          "brand",
+          "equals",
+          "Volvo"
+        ]
+      ]
+    ]);
+
+  verify<Friend>(({ name, tags, age, cars }) =>
 
     tags.includes("friend").AND(
       name.equals("Arne").ignoreCase()
-    ).AND(cars.every(({model}) => model.equals("V70").ignoreCase()))
-    .AND(age.above(1)), [[[
-    [
-      "tags",
-      "includes",
-      "friend"
+    ).AND(cars.every(({ model }) => model.equals("V70").ignoreCase()))
+      .AND(age.above(1)), [[[
+        [
+          "tags",
+          "includes",
+          "friend"
+        ],
+        "AND",
+        [
+          "name",
+          "equals",
+          "Arne",
+          "ignoreCase"
+        ]
+      ],
+        "AND",
+      [
+        "cars",
+        "every",
+        [
+          "model",
+          "equals",
+          "V70",
+          "ignoreCase"
+        ]
+      ]
+      ],
+      "AND",
+      [
+        "age",
+        "above",
+        1
+      ]
+    ]);
+  verify<Friend>(f => NOT(f.name.anyOf(["Foo", "Bar"])),[
+    "name",
+    ["NOT", "anyOf"],
+    ["Foo", "Bar"]
+  ]);
+
+  verify<Friend>(f => NOT(f.age.above(3).AND(f.age.belowOrEqual(7))), [[
+    "age",
+    "above",
+    3
+  ],
+  ["NOT", "AND"],
+  [
+    "age",
+    "belowOrEqual",
+    7
+  ]
+  ]);
+
+
+  const prefixes = ["Foo", "Bar", "Apa"];
+  verify<Friend>(({name}) =>
+    prefixes.map(prefix => name.startsWith(prefix) as Expression<Friend>).reduce((res, expr) => res.AND(expr)),[
+      [
+      ["name", "startsWith", "Foo"],
+      "AND",
+      ["name", "startsWith", "Bar"]
     ],
     "AND",
     [
-      "name",
-      "equals",
-      "Arne",
-      "ignoreCase"
-    ]
-  ],
-  "AND",
-  [
-    "cars",
-    "every",
-    [
-      "model",
-      "equals",
-      "V70",
-      "ignoreCase"
-    ]
-  ]
-],
-"AND",
-[
-  "age",
-  "above",
-  1
-]
-])
+      "name", "startsWith", "Apa"
+    ]]);
 });
